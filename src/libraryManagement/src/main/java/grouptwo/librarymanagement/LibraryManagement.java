e769265d68d4b809a0361064cc65902b645f89a0
Lân Octavian <lantruongtuan333@gmail.com>
2024-11-18 22:01:00 +0700

    Add NETBEAN project

---
+++ src/libraryManagement/src/main/java/grouptwo/librarymanagement/LibraryManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grouptwo.librarymanagement;
+
+/**
+ *
+ * @author lantr
+ */
+public class LibraryManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
