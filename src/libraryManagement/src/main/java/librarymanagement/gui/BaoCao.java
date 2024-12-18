a367ad78a3ccfd4587fd891c0c7bd8b99133757d
minhphat1612 <minhphat1612@gmail.com>
2024-12-18 17:19:01 +0700

    change

--- src/libraryManagement/src/main/java/librarymanagement/gui/BaoCao.java
+++ src/libraryManagement/src/main/java/librarymanagement/gui/BaoCao.java
@@ -173,11 +173,11 @@ public class BaoCao extends javax.swing.JPanel {
 
             },
             new String [] {
-                "STT", "Tên", "Tên Sách", "Tình Trạng", "Số ngày hạn", "Thông số"
+                "Tên", "Tên Sách", "Tình Trạng", "Số ngày hạn", "Thông số"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
             };
 
             public Class getColumnClass(int columnIndex) {
